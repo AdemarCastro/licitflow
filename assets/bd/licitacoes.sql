506a3071b1d4520fae6f9678d898a9ba16b4f903
AdemarCastro <ademar.castro.fh@gmail.com>
2024-04-30 14:26:32 -0400

    [FEAT] - Ajustes no script SQL em

--- assets/bd/licitacoes.sql
+++ assets/bd/licitacoes.sql
@@ -1,71 +1,53 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Tempo de geração: 23/04/2024 às 12:49
--- Versão do servidor: 8.0.36-0ubuntu0.20.04.1
--- Versão do PHP: 7.4.33
+CREATE DATABASE IF NOT EXISTS licitflow
+DEFAULT CHARACTER SET utf8mb4
+DEFAULT COLLATE utf8mb4_general_ci
+;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+# Visualizar Banco
+# SHOW DATABASES;
 
+# Usar Banco de Dados
+USE licitflow;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    cpf VARCHAR(14) NOT NULL UNIQUE,
+    cep VARCHAR(9) NOT NULL,
+    n_da_casa VARCHAR(10) NOT NULL,
+    rua VARCHAR(100) NOT NULL,
+    estado VARCHAR(50) NOT NULL,
+    cidade VARCHAR(100) NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    nivel_acesso ENUM('admin', 'usuario', 'sem-acesso') NOT NULL DEFAULT 'sem-acesso'
+);
 
---
--- Banco de dados: `licitflow`
---
+CREATE TABLE IF NOT EXISTS licitacoes (
+  id int NOT NULL AUTO_INCREMENT,
+  titulo varchar(255) NOT NULL,
+  servico varchar(255) NOT NULL,
+  descricao text NOT NULL,
+  documento varchar(255) DEFAULT NULL,
+  data_criacao timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
+);
 
--- --------------------------------------------------------
+-- Exemplo 1: Construção de Escola Municipal
+INSERT INTO licitacoes (titulo, servico, descricao, documento, data_criacao) 
+VALUES ('Construção de Escola Municipal na Zona Norte', 'Construção Civil', 
+        'A prefeitura de nossa cidade está buscando propostas para a construção de uma escola municipal na zona norte. O projeto inclui a construção de salas de aula, laboratórios, quadras esportivas, e áreas administrativas.', 
+        'Edital de Licitação Nº 001/2024', '2024-04-20');
 
---
--- Estrutura para tabela `licitacoes`
---
+-- Exemplo 2: Fornecimento de Equipamentos Médicos
+INSERT INTO licitacoes (titulo, servico, descricao, documento, data_criacao) 
+VALUES ('Fornecimento de Equipamentos Médicos para Hospital Municipal', 'Equipamentos Médicos', 
+        'O hospital municipal está buscando fornecedores para a aquisição de equipamentos médicos modernos, incluindo aparelhos de raio-x, monitores cardíacos, ventiladores pulmonares, e outros equipamentos essenciais para o atendimento médico de qualidade.', 
+        'Edital de Licitação Nº 002/2024', '2024-04-21');
 
-CREATE TABLE `licitacoes` (
-  `id` int NOT NULL,
-  `titulo` varchar(255) NOT NULL,
-  `servico` varchar(255) NOT NULL,
-  `descricao` text NOT NULL,
-  `documento` varchar(255) DEFAULT NULL,
-  `data_criacao` timestamp NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- Exemplo 3: Serviços de Manutenção de Estradas
+INSERT INTO licitacoes (titulo, servico, descricao, documento, data_criacao) 
+VALUES ('Serviços de Manutenção de Estradas Rurais', 'Manutenção de Estradas', 
+        'A prefeitura está buscando empresas para realizar serviços de manutenção em estradas rurais do município. Isso inclui reparo de buracos, limpeza das vias, sinalização, e outros serviços de conservação.', 
+        'Edital de Licitação Nº 003/2024', '2024-04-22');
 
---
--- Despejando dados para a tabela `licitacoes`
---
-
-INSERT INTO `licitacoes` (`id`, `titulo`, `servico`, `descricao`, `documento`, `data_criacao`) VALUES
-(3, 'Titulo 04', 'Servico 04', 'JODIA JDAA JDOI AODJ JA IOD JAIOD OAID', '20_at2_versionamentoDeCod.png', '2024-04-23 14:47:12'),
-(4, 'Titulo 03', 'Servico 03', 'adju ad jaod oai  JIOAJDIOAJDIOAJIODAIO DJDOJAIOSD', '10_at2_versionamentoDeCodigo.png', '2024-04-23 14:48:24'),
-(5, 'Titulo 05', 'Servico 05', ' DJOAI DA Djioa jdioa djioajsd AOID JOAIJDADAJDIO AJOIDJ AODJOAI D', '11_at2_versionamentoDeCodigo.png', '2024-04-23 14:52:57');
-
---
--- Índices para tabelas despejadas
---
-
---
--- Índices de tabela `licitacoes`
---
-ALTER TABLE `licitacoes`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT para tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `licitacoes`
---
-ALTER TABLE `licitacoes`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
